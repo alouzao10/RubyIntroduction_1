721ec852b4d44aeebfb4a53a746b606252367919
alouzao10 <alouzao10@gmail.com>
2020-06-10 15:13:04 -0400

    Starting with Ruby

---
+++ helloRuby.rb
@@ -0,0 +1,36 @@
+# This is my Ruby file
+=begin
+    
+This is where I will learn some Ruby
+Then I'll do Ruby on Rails...    
+
+=end
+
+# Output Items
+puts "Hello World"
+puts "This is Ruby code. "
+print "/ | /"
+puts "/ | /"
+
+# Variables and Output
+name = "Paul"
+height = "10" 
+puts ("There once was a man named " + name)
+puts ("He was " + height + " feet tall")
+name = "Sally"
+height = "5" 
+puts "But she didn't like being #{height} feet tall"
+puts "She hates being #{name}"
+
+# Variables and Data Types
+name = "Mike" #String
+age = 75 #Int
+isAlive = true #Bool
+netWorth = 5563825.55 #Float
+flaws = nil #Nil = no value
+
+#Working with Strings
+
+
+
+
